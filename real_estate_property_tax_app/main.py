c54c4614e91c66793956ec5492333cc82155157e
talha-ayub-qbatch <talha.ayub@qbatch.com>
2023-12-16 19:14:32 +0500

    update

--- real_estate_property_tax_app/main.py
+++ real_estate_property_tax_app/main.py
@@ -1,4 +1,7 @@
 from app import app
+from flask_cors import CORS
 
 if __name__ == '__main__':
+    CORS(app, resources={r"/": {"origins": ""}}, supports_credentials=True,
+        methods=["GET", "HEAD", "POST", "PATCH", "PUT", "DELETE"])
     app.run(debug=True)
