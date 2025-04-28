43c4c6f3a04f5da55642f85e82f1ecb637123f17
Talha-Ayub998 <talhaayub998@gmail.com>
2025-04-28 22:07:01 +0500

    Update r_code_2

--- real_estate_property_tax_app/r_code/r_code_2.py
+++ real_estate_property_tax_app/r_code/r_code_2.py
@@ -1,46 +1,17 @@
 r_code_2 = """
 
-perform_analysis <- function(atr_calc, values) {
-  # Combine the two datasets into a single dataset
-  merged_data <- atr_calc
-  merged_data$v1 <- as.numeric(as.character(merged_data$v1))
-  merged_data$v2 <- as.numeric(as.character(merged_data$v2))
-  merged_data$v3 <- as.numeric(as.character(merged_data$v3))
-  merged_data$prop_val <- as.numeric(as.character(merged_data$prop_val))
-  merged_data$group_type <- as.numeric(as.character(merged_data$group_type))
-  merged_data$atr <- as.numeric(as.character(merged_data$atr))
-  
-  # Calculate the average atr for each value_bin when group_type is 1
-  average_atr <- with(merged_data, tapply(atr[group_type == 1], value_bin[group_type == 1], mean))
-  
-  # Replace atr values where atr is 0 and group_type is 1 with the average atr for the same value_bin
-  merged_data$atr[merged_data$atr == 0 & merged_data$group_type == 1] <- with(merged_data, average_atr[value_bin[atr == 0 & group_type == 1]])
-  
-  # Check if all values within a value_bin are 0 and drop those observations
-  to_drop <- with(merged_data, tapply(atr, value_bin, function(x) all(x == 0)))
-  merged_data <- merged_data[!(merged_data$value_bin %in% names(to_drop) & to_drop[as.character(merged_data$value_bin)]), ]
-  
-  # Check if the total number of observations for group_type == 1 is less than 12
-  #  if (sum(merged_data$group_type == 1) < 12) {
-  #    message("We can't proceed further. Thanks.")
-  #  } else {
-  #    # Ensure that the result is numeric
-  #    merged_data$atr <- as.numeric(as.character(merged_data$atr))
-  #    # Continue with further processing if needed
-  #  }
-  
-  
+perform_analysis <- function(atr_calc) {
+
 # 4. Run the restricted spline
-  
 ## 4.1 grab the knots of the spline implied in the vs in the survey responses
-  
-  svy_dta <- merged_data
+
+  svy_dta <- atr_calc
 
   svy_dta$lprop_val <-
     log(svy_dta$prop_val)
   k1 <- svy_dta %>%
     subset(
-      v2 != 0 & 
+      v2 != 0 &
         v3 == 0
     ) %>%
     mutate(
